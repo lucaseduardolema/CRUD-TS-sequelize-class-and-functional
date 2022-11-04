7a9ebafbdab3f2d20d9c1f8587fa6603be477fca
Lucas Eduardo <lucaseduardolema@gmail.com>
2022-11-03 23:27:20 -0300

    Delete build directory

--- build/middlewares/handleError.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const handleError = (error, _req, res, _next) => {
-    const { status, message } = error;
-    res.status(status || 500).json({ message });
-};
-exports.default = handleError;
