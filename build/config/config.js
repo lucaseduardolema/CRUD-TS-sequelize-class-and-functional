20bea462e201cdf14912ac22fdc9e1564d069161
lucaseduardolema <lucaseduardolema@gmail.com>
2022-11-03 23:25:22 -0300

    change db name

--- build/config/config.js
+++ build/config/config.js
@@ -3,7 +3,7 @@ require("dotenv/config");
 const config = {
     username: process.env.DB_USER || "root",
     password: process.env.DB_PASS || "root",
-    database: process.env.DB_NAME || "books_api",
+    database: process.env.DB_NAME || "TypeScriptExpress",
     host: process.env.DB_HOST || "localhost",
     port: Number(process.env.DB_PORT) || 3306,
     dialect: "mysql",
